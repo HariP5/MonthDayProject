fc3d037a87d2f13936a0a3dcb7940dce6e6df06a
HariP5 <hari.pokhrel.2020@student.lhschools.org>
2019-11-21 20:34:22 -0500

    Added Scanner

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What year were you born:");
+        int year = input.nextInt();
+        System.out.print("What month were you born in:");
+        int month = input.nextInt();
+        System.out.print("What day were you born in:");
+        int dayOfMonth = input.nextInt();
+
+
+    }
+
+
+}
